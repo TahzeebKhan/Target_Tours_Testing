e11ca5a094da752c5240f4db43dbb618192ede3c
mishramukul8923 <mukul.mishra@webninjaz.com>
2025-12-12 19:31:31 +0530

    fix carousel

--- src/app/home-page/components/popularFlights/PopularFlights.jsx
+++ src/app/home-page/components/popularFlights/PopularFlights.jsx
@@ -1,169 +1,146 @@
-// import React from 'react'
-// import styles from './PopularFlights.module.css'
 
-// const PopularFlights = () => {
-
-//     // --- Flight Card Data ---
-//     const cardData = [
-//         {
-//             id: 1,
-//             img: "/images/items1.jpg",
-//             city: "Hyderabad",
-//             date: "24 Mar 2026 - 14 Apr 2026",
-//             price: "₹20,000"
-//         },
-//         {
-//             id: 2,
-//             img: "/images/items1.jpg",
-//             city: "Hyderabad",
-//             date: "24 Mar 2026 - 14 Apr 2026",
-//             price: "₹20,000"
-//         },
-//         {
-//             id: 3,
-//             img: "/images/items1.jpg",
-//             city: "Hyderabad",
-//             date: "24 Mar 2026 - 14 Apr 2026",
-//             price: "₹20,000"
-//         },
-//         {
-//             id: 4,
-//             img: "/images/items1.jpg",
-//             city: "Hyderabad",
-//             date: "24 Mar 2026 - 14 Apr 2026",
-//             price: "₹20,000"
-//         }
-//     ];
-
-//     return (
-//         <section className={styles.section}>
-//             <div className={styles.container}>
-
-//                 {/* Heading Section */}
-//                 <div className={styles.heading}>
-//                     Popular Flights to Destination From
-//                     <div className={styles.headingMult}>
-//                         <span> Delhi</span>
-//                         <img src="/icons/dropdown.svg" alt="" />
-//                     </div>
-//                 </div>
-
-//                 {/* Tabs */}
-//                 <nav className={styles.tabsWrap}>
-//                     <ul className={styles.tabs}>
-//                         {['Domestic', 'International'].map((t, i) => (
-//                             <li key={t} className={`${styles.tab} ${i === 0 ? styles.active : ''}`}>
-//                                 <button className={styles.tabBtn}>{t}</button>
-//                             </li>
-//                         ))}
-//                     </ul>
-//                 </nav>
-
-//                 {/* Carousel Cards */}
-//                 <div className={styles.carouselContainerMain}>
-//                     <div className={styles.carouselContainer}>
-
-//                         {cardData.map((item) => (
-//                             <div key={item.id} className={styles.itemsCard}>
-//                                 <img src={item.img} alt="" />
-
-//                                 {/* For first card only - show text overlay */}
-//                                 {item.city && (
-//                                     <>
-//                                         <div className={styles.overlay}></div>
-
-//                                         <div className={styles.textContainer}>
-//                                             <div className={styles.textTop}>
-//                                                 <span>{item.city}</span>
-//                                                 <p>{item.date}</p>
-//                                             </div>
-
-//                                             <div className={styles.textBottom}>
-//                                                 <p className={styles.economy}>
-//                                                     Economy From <span>{item.price}</span>
-//                                                 </p>
-//                                                 <p className={styles.discoverText}>DISCOVER FLIGHTS</p>
-//                                             </div>
-//                                         </div>
-//                                     </>
-//                                 )}
-//                             </div>
-//                         ))}
-
-//                     </div>
-//                 </div>
-
-//                 {/* Buttons */}
-//                 <div className={styles.btnContainer}>
-//                     <div className={styles.btn}>
-//                         <img src="/icons/left.svg" alt="" />
-//                     </div>
-//                     <div className={styles.btn}>
-//                         <img src="/icons/right.svg" alt="" />
-//                     </div>
-//                 </div>
-
-//             </div>
-//         </section>
-//     )
-// }
-
-// export default PopularFlights
 
 "use client"
 import React, { useState } from 'react'
 import styles from './PopularFlights.module.css'
-import { Virtual, Navigation, Pagination } from 'swiper/modules'
-import { Swiper, SwiperSlide } from 'swiper/react' // Fixed import
-import InnerCarousel from '@/app/exploreCarousel/component/InnerCarousel'
+import { Navigation } from 'swiper/modules'
+import { Swiper, SwiperSlide } from 'swiper/react'
 
 // Import Swiper styles
 import 'swiper/css'
 import 'swiper/css/navigation'
-import 'swiper/css/pagination'
 
 const PopularFlights = () => {
     const [swiperRef, setSwiperRef] = useState(null)
-    // Create array with 10 slides
-    const [slides] = useState(
-        Array.from({ length: 4}).map((_, index) => `Slide ${index + 1}`)
-    )
-
-    const slideTo = (index) => {
-        if (swiperRef) {
-            swiperRef.slideTo(index - 1, 0)
-        }
-    }
-    const cardData = [
+    const [activeTab, setActiveTab] = useState('Domestic')
+    
+    const domesticData = [
         {
             id: 1,
-            img: "/images/items1.jpg",
+            img: "/images/hyderabad.png",
             city: "Hyderabad",
             date: "24 Mar 2026 - 14 Apr 2026",
             price: "₹20,000"
         },
         {
             id: 2,
-            img: "/images/items1.jpg",
-            city: "Hyderabad",
+            img: "/images/chennai2.png",
+            city: "Chennai",
             date: "24 Mar 2026 - 14 Apr 2026",
             price: "₹20,000"
         },
         {
             id: 3,
-            img: "/images/items1.jpg",
-            city: "Hyderabad",
+            img: "/images/pune2.png",
+            city: "Pune",
             date: "24 Mar 2026 - 14 Apr 2026",
             price: "₹20,000"
         },
         {
             id: 4,
+            img: "/images/ahamdabad2.png",
+            city: "Ahmedabad",
+            date: "24 Mar 2026 - 14 Apr 2026",
+            price: "₹20,000"
+        },
+        {
+            id: 5,
             img: "/images/items1.jpg",
-            city: "Hyderabad",
+            city: "Mumbai",
+            date: "24 Mar 2026 - 14 Apr 2026",
+            price: "₹20,000"
+        },
+        {
+            id: 6,
+            img: "/images/pune2.png",
+            city: "Bangalore",
+            date: "24 Mar 2026 - 14 Apr 2026",
+            price: "₹20,000"
+        },
+        {
+            id: 7,
+            img: "/images/items1.jpg",
+            city: "Kolkata",
+            date: "24 Mar 2026 - 14 Apr 2026",
+            price: "₹20,000"
+        },
+        {
+            id: 8,
+            img: "/images/items1.jpg",
+            city: "Delhi",
             date: "24 Mar 2026 - 14 Apr 2026",
             price: "₹20,000"
         }
     ];
+
+    const internationalData = [
+        {
+            id: 1,
+            img:"/images/ahamdabad2.png",
+            city: "Dubai",
+            date: "24 Mar 2026 - 14 Apr 2026",
+            price: "₹45,000"
+        },
+        {
+            id: 2,
+            img:"/images/hyderabad.png",
+            city: "Singapore",
+            date: "24 Mar 2026 - 14 Apr 2026",
+            price: "₹35,000"
+        },
+        {
+            id: 3,
+            img: "/images/chennai2.png",
+            city: "Bangkok",
+            date: "24 Mar 2026 - 14 Apr 2026",
+            price: "₹28,000"
+        },
+        {
+            id: 4,
+            img: "/images/items1.jpg",
+            city: "London",
+            date: "24 Mar 2026 - 14 Apr 2026",
+            price: "₹85,000"
+        },
+        {
+            id: 5,
+            img: "/images/items1.jpg",
+            city: "New York",
+            date: "24 Mar 2026 - 14 Apr 2026",
+            price: "₹95,000"
+        },
+        {
+            id: 6,
+            img: "/images/items1.jpg",
+            city: "Paris",
+            date: "24 Mar 2026 - 14 Apr 2026",
+            price: "₹80,000"
+        },
+        {
+            id: 7,
+            img: "/images/items1.jpg",
+            city: "Tokyo",
+            date: "24 Mar 2026 - 14 Apr 2026",
+            price: "₹75,000"
+        },
+        {
+            id: 8,
+            img: "/images/items1.jpg",
+            city: "Sydney",
+            date: "24 Mar 2026 - 14 Apr 2026",
+            price: "₹90,000"
+        }
+    ];
+
+    const cardData = activeTab === 'Domestic' ? domesticData : internationalData;
+
+    const handleTabChange = (tab) => {
+        setActiveTab(tab);
+        // Reset swiper to first slide when tab changes
+        if (swiperRef) {
+            swiperRef.slideTo(0, 0);
+        }
+    };
 
     return (
         <section className={styles.section}>
@@ -178,62 +155,63 @@ const PopularFlights = () => {
 
                 <nav className={styles.tabsWrap}>
                     <ul className={styles.tabs}>
-                        {['Domestic', 'International'].map((t, i) => (
-                            <li key={t} className={`${styles.tab} ${i === 0 ? styles.active : ''}`}>
-                                <button className={styles.tabBtn}>{t}</button>
+                        {['Domestic', 'International'].map((t) => (
+                            <li 
+                                key={t} 
+                                className={`${styles.tab} ${activeTab === t ? styles.active : ''}`}
+                            >
+                                <button 
+                                    className={styles.tabBtn}
+                                    onClick={() => handleTabChange(t)}
+                                >
+                                    {t}
+                                </button>
                             </li>
                         ))}
                     </ul>
                 </nav>
 
 
-                <div style={{ maxWidth: "1520px", margin: "0 auto" }} className='ExpCarouselWrapper'>
+                <div style={{ maxWidth: "1520px", margin: "0 auto" }} className='popularFlightsCarouselWrapper'>
                     <Swiper
-                        modules={[Virtual, Navigation]}
+                        key={activeTab}
+                        modules={[Navigation]}
                         onSwiper={setSwiperRef}
-                        slidesPerView={1}
-                        centeredSlides={false}
+                        slidesPerView={4}
+                        slidesPerGroup={1}
                         spaceBetween={30}
-                        pagination={{
-                            type: 'fraction',
-                        }}
                         navigation={true}
-                        virtual
+                        loop={true}
+                        loopAdditionalSlides={2}
                     >
-                        {slides.map((slideContent, index) => (
-                            <SwiperSlide key={slideContent} virtualIndex={index}>
-                                {/* <div style={{ margin: "100px auto" }}> */}
+                        {cardData.map((item, index) => (
+                            <SwiperSlide key={item.id}>
                                 <div className={styles.carouselContainer}>
+                                    <div className={styles.itemsCard}>
+                                        <img src={item.img} alt="" />
 
-                                    {cardData.map((item) => (
-                                        <div key={item.id} className={styles.itemsCard}>
-                                            <img src={item.img} alt="" />
+                                        {/* Show text overlay on all cards */}
+                                        {item.city && (
+                                            <>
+                                                <div className={styles.overlay}></div>
 
-                                            {/* For first card only - show text overlay */}
-                                            {item.city && (
-                                                <>
-                                                    <div className={styles.overlay}></div>
-
-                                                    <div className={styles.textContainer}>
-                                                        <div className={styles.textTop}>
-                                                            <span>{item.city}</span>
-                                                            <p>{item.date}</p>
-                                                        </div>
-
-                                                        <div className={styles.textBottom}>
-                                                            <p className={styles.economy}>
-                                                                Economy From <span>{item.price}</span>
-                                                            </p>
-                                                            <p className={styles.discoverText}>DISCOVER FLIGHTS</p>
-                                                        </div>
+                                                <div className={styles.textContainer}>
+                                                    <div className={styles.textTop}>
+                                                        <span>{item.city}</span>
+                                                        <p>{item.date}</p>
                                                     </div>
-                                                </>
-                                            )}
-                                        </div>
-                                    ))}
 
+                                                    <div className={styles.textBottom}>
+                                                        <p className={styles.economy}>
+                                                            Economy From <span>{item.price}</span>
+                                                        </p>
+                                                        <p className={styles.discoverText}>DISCOVER FLIGHTS</p>
+                                                    </div>
+                                                </div>
+                                            </>
+                                        )}
+                                    </div>
                                 </div>
-                                {/* </div> */}
                             </SwiperSlide>
                         ))}
                     </Swiper>
@@ -248,11 +226,25 @@ const PopularFlights = () => {
                     </div>
                 </div> */}
                 <div className={styles.btnContainer}>
-                    <div className={styles.btn} onClick={() => swiperRef?.slidePrev()}>
+                    <div 
+                        className={styles.btn} 
+                        onClick={() => {
+                            if (swiperRef) {
+                                swiperRef.slidePrev();
+                            }
+                        }}
+                    >
                         <img src="/icons/left.svg" alt="" />
                     </div>
 
-                    <div className={styles.btn} onClick={() => swiperRef?.slideNext()}>
+                    <div 
+                        className={styles.btn} 
+                        onClick={() => {
+                            if (swiperRef) {
+                                swiperRef.slideNext();
+                            }
+                        }}
+                    >
                         <img src="/icons/right.svg" alt="" />
                     </div>
                 </div>
