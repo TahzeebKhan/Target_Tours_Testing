a5f4eb3f5f6edd6c2ebbc74c5f225dfe6b954d43
ayushsharma1603 <ayushsharmapro18@gmail.com>
2025-12-20 14:04:10 +0530

    context created for flights triptype

--- src/app/flights/layout.js
+++ src/app/flights/layout.js
@@ -1,35 +1,42 @@
+"use client";
 import Navbar from "./Navbar";
 import styles from "./FlightsLayout.module.css";
 import DatePriceSlider from "./components/DatePriceSlider";
 import FlightFilters from "./components/FlightsFilters";
 import TopFilterSection from "./components/TopFilterSection";
+import { useState } from "react";
+import { TripTypeProvider } from "./TripTypeContext";
 
 export default function FlightsLayout({ children }) {
+  // const [tripType, setTripType] = useState("oneway");
   return (
     <>
-      {/* Top Navbar */}
-      <div className={styles.wrapper}>
-        <Navbar />
-        <div className={styles.imageBackgound}>
-          <TopFilterSection />
-        </div>
-      </div>
-      {/* Page Wrapper */}
-      <main className={styles.page}>
-        <div className={styles.container}>
-          {/* top date slider */}
-          <div className={styles.dateSlider}>
-            <DatePriceSlider />
+      <TripTypeProvider>
+        {" "}
+        {/* Top Navbar */}
+        <div className={styles.wrapper}>
+          <Navbar />
+          <div className={styles.imageBackgound}>
+            <TopFilterSection />
           </div>
-          {/* Sidebar */}
-          <aside className={styles.sidebar}>
-            <FlightFilters />
-          </aside>
-
-          {/* Main content */}
-          <section className={styles.content}>{children}</section>
         </div>
-      </main>
+        {/* Page Wrapper */}
+        <main className={styles.page}>
+          <div className={styles.container}>
+            {/* top date slider */}
+            <div className={styles.dateSlider}>
+              <DatePriceSlider />
+            </div>
+            {/* Sidebar */}
+            <aside className={styles.sidebar}>
+              <FlightFilters />
+            </aside>
+
+            {/* Main content */}
+            <section className={styles.content}>{children}</section>
+          </div>
+        </main>
+      </TripTypeProvider>
     </>
   );
 }
--- src/app/flights/page.js
+++ src/app/flights/page.js
@@ -1,11 +1,27 @@
+"use client";
 import FlightBooking from "../flightBookings/components/FlightBooking/FlightBooking";
+import { useTripType } from "./TripTypeContext";
 
 const page = () => {
-    return (
+  const { tripType } = useTripType();
+  if (!tripType) return null;
+  return (
+    <>
+      {tripType === "oneway" && <FlightBooking />}
+      {tripType === "round" && (
         <>
-        <FlightBooking/>
+          {/* yaha roundtrip component aega roundtrip booking component */}
+          roundtrip
         </>
-    );
-}
+      )}
+      {tripType === "multi" && (
+        <>
+          {/* yaha multi component aega multi booking component */}
+          multitrip
+        </>
+      )}
+    </>
+  );
+};
 
 export default page;
