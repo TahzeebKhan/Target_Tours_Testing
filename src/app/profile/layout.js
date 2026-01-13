22a32d7d19f0a6ac7c604a0b2ea11ee8ea054275
ayushsharma1603 <ayushsharmapro18@gmail.com>
2026-01-13 10:35:19 +0530

    user details api

--- src/app/profile/layout.js
+++ src/app/profile/layout.js
@@ -1,9 +1,11 @@
-"use client"
-import React from 'react'
-import Navbar from '../hotel-detail/Navbar'
-import SideBar from './components/sideBar/SideBar'
-import styles from './Profile.module.css'
-import { ProfileProvider } from './context/ProfileContext'
+"use client";
+import React from "react";
+import Navbar from "../hotel-detail/Navbar";
+import SideBar from "./components/sideBar/SideBar";
+import styles from "./Profile.module.css";
+import { ProfileProvider } from "./context/ProfileContext";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const ProfileLayout = ({ children }) => {
   return (
@@ -12,13 +14,23 @@ const ProfileLayout = ({ children }) => {
       <div className={styles.profileContainer}>
         <ProfileProvider>
           <SideBar />
-          <div className={styles.profileContent}>
-            {children}
-          </div>
+          <div className={styles.profileContent}>{children}</div>
         </ProfileProvider>
       </div>
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop
+        closeOnClick
+        pauseOnHover
+        theme="light"
+        toastStyle={{
+          borderRadius: "0px",
+        }}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default ProfileLayout
+export default ProfileLayout;
