24cd58c12c6ba61042238b63febc280f57ad5a36
aditya9525 <adityastar432@gmail.com>
2026-01-15 10:19:06 +0530

    Hotel details mobile View

--- src/app/hotel-list/page.js
+++ src/app/hotel-list/page.js
@@ -3,30 +3,36 @@ import styles from "./layout.module.css";
 import TourHeroSection from "./components/tourHeroSection.js/TourHeroSection";
 import TourListing from "./components/tourListing/TourListing";
 import MapSection from "./components/mapSection/MapSection";
+import MobileHotelDetails from "./components/mobileView/MobileHotelDetails";
 
 
 const HotelList = () => {
   return (
     <>
       {/* HERO SECTION (Image + Search) */}
-      <Suspense fallback={<div>Loading...</div>}>
-        <TourHeroSection />
-      </Suspense>
+      <div className={styles.tourListSectionWrappper}>
+        <Suspense fallback={<div>Loading...</div>}>
+          <TourHeroSection />
+        </Suspense>
 
-      {/* MAIN CONTENT */}
-      <section className={styles.tourContent}>
-        <div className={styles.tourLayout}>
-          {/* LEFT: FILTERS */}
-          <aside className={styles.tourFilters}>
-            <TourListing />
-          </aside>
+        {/* MAIN CONTENT */}
+        <section className={styles.tourContent}>
+          <div className={styles.tourLayout}>
+            {/* LEFT: FILTERS */}
+            <aside className={styles.tourFilters}>
+              <TourListing />
+            </aside>
 
-          {/* RIGHT: RESULTS GRID */}
-          <div className={styles.tourResults}>
-            <MapSection />
+            {/* RIGHT: RESULTS GRID */}
+            <div className={styles.tourResults}>
+              <MapSection />
+            </div>
           </div>
-        </div>
-      </section>
+        </section>
+      </div>
+      <div className={styles.tourListSectionMobileView}>
+        <MobileHotelDetails />
+      </div>
     </>
   );
 };
